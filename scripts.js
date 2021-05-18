9168ac26f802fd20246f7eb1d0e95adca979d04c
GilbertoASJ <alvesgilberto84@gmail.com>
2021-05-18 17:19:20 -0300

    :sparkles: Creating scripts

---
+++ scripts.js
@@ -0,0 +1,31 @@
+/* Function to get address by CEP */
+
+function getAddress(cep) {
+
+	let url = `https://viacep.com.br/ws/${cep}/json/`
+	
+	let xmlHttp = new XMLHttpRequest();
+	xmlHttp.open('GET', url);
+
+	xmlHttp.onreadystatechange = () => {
+
+		if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+			
+			let jsonDataText = xmlHttp.responseText;
+			let jsonDataObject = JSON.parse(jsonDataText)
+
+			console.log(jsonDataObject)
+
+			document.querySelector('#endereco').value = jsonDataObject.logradouro
+			document.querySelector('#bairro').value = jsonDataObject.bairro
+			document.querySelector('#cidade').value = jsonDataObject.localidade
+			document.querySelector('#uf').value = jsonDataObject.uf
+			document.querySelector('#ddd').value = jsonDataObject.ddd
+
+		}
+
+	}
+
+	xmlHttp.send();
+	
+}
